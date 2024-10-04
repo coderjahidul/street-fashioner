9d3486fd97358e6cd29837c478316b91651162d8
coderjahidul <sobuz0349@gmail.com>
2024-10-04 19:41:53 +0600

    add all file and defendant in street-fashioner theme

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Theme Functions File
+ *
+ * This file contains functions and hooks that enhance the functionality of the theme.
+ */
+
+// Add Theme Support
+function theme_setup() {
+    // Add support for featured images
+    add_theme_support('post-thumbnails');
+
+    // Add support for custom logo
+    add_theme_support('custom-logo');
+
+    // Add support for custom menus
+    register_nav_menus(array(
+        'primary_menu' => __('Primary Menu', 'theme-textdomain'),
+    ));
+
+    // Add support for title tag
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'theme_setup');
+
+// Enqueue styles
+function my_theme_enqueue_styles() {
+    // Google Font
+    wp_enqueue_style('google-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800', false);
+
+    // CSS Files
+    wp_enqueue_style('prettyPhoto-css', get_template_directory_uri() . '/css/prettyPhoto.css');
+    wp_enqueue_style('camera-css', get_template_directory_uri() . '/css/camera.css');
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.css');
+    wp_enqueue_style('theme-css', get_template_directory_uri() . '/css/theme.css');
+    wp_enqueue_style('skin-css', get_template_directory_uri() . '/css/skins/tango/skin.css');
+    wp_enqueue_style('bootstrap-responsive-css', get_template_directory_uri() . '/css/bootstrap-responsive.css');
+
+    // HTML5 Shim for IE < 9
+    wp_enqueue_script('html5-shiv', 'http://html5shim.googlecode.com/svn/trunk/html5.js');
+    wp_script_add_data('html5-shiv', 'conditional', 'lt IE 9');
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+
+// require files customize function file
+require_once('customize-function.php');
